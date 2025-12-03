86ea5b91d80d8ad3ee91d47617509b3b3ca071a5
Dennis Lynch <lynch.dennis94@gmail.com>
2025-12-03 09:36:06 -0500

    Implements day three

--- day_three.go
+++ day_three.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -29,13 +30,68 @@ func (d *DayThree) LoadFile() {
 		log.Fatal(err)
 	}
 
-	d.args = strings.Split(string(dat), ",")
+	d.args = strings.Split(string(dat), "\n")
+}
+
+func findLargestDigitInRange(bank string, start, end int) (int, int) {
+	maxDigitPlace := -1
+	maxDigit := -1
+	for i := range end - start {
+		currentDigitString := string(bank[i+start])
+		currentDigit, err := strconv.Atoi(currentDigitString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// We want maximum search space for later searches, so only update if definitively larger
+		if currentDigit > maxDigit {
+			maxDigit = currentDigit
+			maxDigitPlace = i
+		}
+
+		// We won't find higher than 9, so stop
+		if currentDigit == 9 {
+			break
+		}
+	}
+
+	return maxDigit, maxDigitPlace + start
+}
+
+func findJoltage(bank string, batteryCount int) int {
+	startingPlace := 0
+	joltageString := ""
+
+	// Loop through each battery we need to turn on
+	for i := range batteryCount {
+		// End is defined to allow for enough room for remaining digits to be found
+		digit, place := findLargestDigitInRange(bank, startingPlace, len(bank)+i-batteryCount+1)
+
+		// Start one _after_ where we just stopped on the next iteration
+		startingPlace = place + 1
+		joltageString += strconv.Itoa(digit)
+	}
+
+	// Convert the built-up string back into an int, and return
+	if joltage, err := strconv.Atoi(joltageString); err != nil {
+		log.Fatal(err)
+	} else {
+		return joltage
+	}
+	return 0
 }
 
 func (d *DayThree) SolvePartA() {
-	fmt.Printf("Part A: \n")
+	totalOutputJoltage := 0
+	for _, bank := range d.args {
+		totalOutputJoltage += findJoltage(bank, 2)
+	}
+	fmt.Printf("Part A: %d\n", totalOutputJoltage)
 }
 
 func (d *DayThree) SolvePartB() {
-	fmt.Printf("Part B: \n")
+	totalOutputJoltage := 0
+	for _, bank := range d.args {
+		totalOutputJoltage += findJoltage(bank, 12)
+	}
+	fmt.Printf("Part B: %d\n", totalOutputJoltage)
 }
